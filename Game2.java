60e190ff56b4ef139df39b9a1cb9ad783634aa6b
Александр Скоморохов <skomorohovaleksandr52@gmail.com>
2025-02-16 18:18:51 +0300

    add: isMoveAvailable, checkWin

--- Game2.java
+++ Game2.java
@@ -32,6 +32,44 @@ public class Game2 {
         return board.toString();
     }
 
+    public boolean isMoveAvailable() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tabl[i][j] == Cell.EMPTY) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Cell checkWin() {
+        // проверка строк
+        for (int i = 0; i < 3; i++) {
+            if (tabl[i][0] != Cell.EMPTY && tabl[i][0] == tabl[i][1] && tabl[i][1] == tabl[i][2]) {
+                return tabl[i][0];
+            }
+        }
+
+        // столбцы
+        for (int j = 0; j < 3; j++) {
+            if (tabl[0][j] != Cell.EMPTY && tabl[0][j] == tabl[1][j] && tabl[1][j] == tabl[2][j]) {
+                return tabl[0][j];
+            }
+        }
+
+        //диагонали
+        if (tabl[0][0] != Cell.EMPTY && tabl[0][0] == tabl[1][1] && tabl[1][1] == tabl[2][2]) {
+            return tabl[0][0];
+        }
+
+        if (tabl[0][2] != Cell.EMPTY && tabl[0][2] == tabl[1][1] && tabl[1][1] == tabl[2][0]) {
+            return tabl[0][2];
+        }
+
+        return Cell.EMPTY;
+    }
+
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в игру крестики-нолики!");
         Game2 game = new Game2();
